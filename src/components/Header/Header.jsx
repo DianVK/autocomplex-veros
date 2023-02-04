62a25e723521e02cad613e0ef2f26390e0eea2c5
DianVK <diankinanev@abv.bg>
2023-02-04 12:14:56 +0200

    menu added
    menu added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,55 @@
-import React from 'react'
-import css from './Header.module.scss'
+import React, { useState } from "react";
+import css from "./Header.module.scss";
+import { BiMenuAltRight, BiPhoneCall } from "react-icons/bi";
+import { motion } from "framer-motion";
+import { headerVariants, getMenuStyles } from "../../utils/motion";
+import useHeaderShadow from "../../hooks/useHeaderShadow";
+
 const Header = () => {
-    return (
-        <div className={css.wrapper}>
+  const [menuOpened, setMenuOpened] = useState(false);
+  const headerShadow = useHeaderShadow();
+  return (
+    <motion.div
+      initial="hidden"
+      whileInView={"show"}
+      variants={headerVariants}
+      viewport={{ once: false, amount: 0.25 }}
+      className={`paddings ${css.wrapper}`}
+      style={{ boxShadow: headerShadow }}
+    >
+      <div className={`flexCenter innerWidth ${css.container}`}>
+        <div className={`${css.name}`}>Автокомплекс "ВЕРОС"</div>
 
-            <div className={css.container}>
-                <div className={css.name}>
-                Автокомплекс "ВЕРОС"
-
-            </div>
-            </div>
+        <ul
+          style={getMenuStyles(menuOpened)}
+          className={`flexCenter ${css.menu}`}
+        >
+          <li>
+            <a href="#">УСЛУГИ</a>
+          </li>
+          <li>
+            <a href="#">ЧИП ТУНИНГ</a>
+          </li>
+          <li>
+            <a href="#">ПЪТНА ПОМОЩ</a>
+          </li>
+          <li>
+            <a href="#">КОНТАКТИ</a>
+          </li>
+          <li className={`flexCenter ${css.phone}`}>
+            <p>+359894407444</p>
+            <BiPhoneCall size={"40px"} />
+          </li>
+        </ul>
+        <div
+          onClick={() => setMenuOpened((prev) => !prev)}
+          className={` ${css.menuIcon}`}
+        >
+          <BiMenuAltRight size={32} />
         </div>
-    )
-}
+      </div>
+    </motion.div>
+  );
+};
 
 export default Header;
